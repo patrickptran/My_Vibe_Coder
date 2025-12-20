d1061efaadad269fabf63f89a3b381143b512d34
MangTran <83230443+MangTran@users.noreply.github.com>
2025-12-20 17:03:41 +0100

    code view, create tabs in Project view, and create file Explorer

--- src/components/file-explorer.tsx
+++ src/components/file-explorer.tsx
@@ -27,10 +27,67 @@ function getLanguageFromExtension(fileName: string): string {
   return extension || "text";
 }
 
+interface FileBreadcrumbProps {
+  filePath: string;
+}
+
+const FileBreadcumb = ({ filePath }: FileBreadcrumbProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 3;
+  const renderBreadcrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      // we can show all the segments
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </Fragment>
+        );
+      });
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegments = pathSegments[pathSegments.length - 1];
+      return (
+        <>
+          <BreadcrumbItem>
+            <span className="text-muted-foreground">{firstSegment}</span>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbEllipsis />
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage className="font-medium">
+              {lastSegments}
+            </BreadcrumbPage>
+          </BreadcrumbItem>
+        </>
+      );
+    }
+  };
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>{renderBreadcrumbItems()}</BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
 interface FileExplorerProps {
   files: FileCollection;
 }
 export const FileExplorer = ({ files }: FileExplorerProps) => {
+  const [copied, setCopied] = useState(false);
   const [selectedFile, setSelectedFile] = useState<string | null>(() => {
     // console.log("Files:", files);
     const fileKeys = Object.keys(files);
@@ -50,6 +107,14 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
     [files]
   );
 
+  const handleCopy = useCallback(() => {
+    if (selectedFile) {
+      navigator.clipboard.writeText(files[selectedFile]);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    }
+  }, [selectedFile, files]);
+
   return (
     <ResizablePanelGroup direction="horizontal">
       <ResizablePanel defaultSize={30} minSize={30} className="bg-sidebar">
@@ -66,16 +131,16 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
         {selectedFile && files[selectedFile] ? (
           <div className="h-full w-full flex flex-col">
             <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
-              {/* TODO File breadcrumb */}
+              <FileBreadcumb filePath={selectedFile} />
               <Hint text="Copy to clipboard" side="bottom">
                 <Button
                   variant="outline"
                   size="icon"
                   className="ml-auto"
-                  onClick={() => {}}
-                  disabled={false}
+                  onClick={handleCopy}
+                  disabled={copied}
                 >
-                  <CopyIcon />
+                  {copied ? <CopyCheckIcon /> : <CopyIcon />}
                 </Button>
               </Hint>
             </div>
--- src/components/tree-view.tsx
+++ src/components/tree-view.tsx
@@ -1,5 +1,23 @@
 import { TreeItem } from "@/types";
 
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { ChevronRightIcon, FileIcon, FolderIcon } from "lucide-react";
+
 interface TreeViewProps {
   data: TreeItem[];
   value?: string | null;
@@ -8,8 +26,81 @@ interface TreeViewProps {
 
 export const TreeView = ({ data, value, onSelect }: TreeViewProps) => {
   return (
-    <div>
-      <span>{JSON.stringify(data)}</span>
-    </div>
+    <SidebarProvider>
+      <Sidebar collapsible="none" className="w-full">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {data.map((item, index) => (
+                  <Tree
+                    key={index}
+                    item={item}
+                    selectedValue={value}
+                    onSelect={onSelect}
+                    parentPath=""
+                  />
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+    </SidebarProvider>
+  );
+};
+
+interface TreeProps {
+  item: TreeItem;
+  selectedValue?: string | null;
+  onSelect: (value: string) => void;
+  parentPath?: string;
+}
+const Tree = ({ item, selectedValue, onSelect, parentPath }: TreeProps) => {
+  const [name, ...items] = Array.isArray(item) ? item : [item];
+  const currentPath = parentPath ? `${parentPath}/${name}` : name;
+
+  if (!items.length) {
+    const isSelected = selectedValue === currentPath;
+    return (
+      <SidebarMenuButton
+        isActive={isSelected}
+        className="data-[active]:bg-accent"
+        onClick={() => onSelect(currentPath)}
+      >
+        <FileIcon />
+        <span className="truncate">{name}</span>
+      </SidebarMenuButton>
+    );
+  }
+
+  return (
+    <SidebarMenuItem>
+      <Collapsible
+        className="group/collapsible [&[data-state=open]>button>svg:first-child]:rotate-90"
+        defaultOpen
+      >
+        <CollapsibleTrigger asChild>
+          <SidebarMenuButton>
+            <ChevronRightIcon className="transition-transform" />
+            <FolderIcon />
+            <span className="truncate">{name}</span>
+          </SidebarMenuButton>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+          <SidebarMenuSub>
+            {items.map((subItem, index) => (
+              <Tree
+                key={index}
+                item={subItem}
+                selectedValue={selectedValue}
+                onSelect={onSelect}
+                parentPath={currentPath}
+              />
+            ))}
+          </SidebarMenuSub>
+        </CollapsibleContent>
+      </Collapsible>
+    </SidebarMenuItem>
   );
 };
