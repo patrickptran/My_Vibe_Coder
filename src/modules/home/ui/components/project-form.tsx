fce784dc4bdeb799121756a6abf2845c4d17875c
MangTran <83230443+MangTran@users.noreply.github.com>
2025-12-23 15:03:08 +0100

    update the home landing page with developed reusable form, add more examples of prompts

---
+++ src/modules/home/ui/components/project-form.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { z } from "zod";
+import { toast } from "sonner";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextareaAutoSize from "react-textarea-autosize";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { cn } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+import { Form, FormField } from "@/components/ui/form";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  value: z.string().min(1, { message: "Value is required" }).max(10000, {
+    message: "Value is too long",
+  }),
+});
+
+export const ProjectForm = () => {
+  const router = useRouter();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(trpc.projects.getMany.queryOptions());
+        router.push(`/projects/${data.id}`);
+        // TODO: invalidate usage status
+      },
+      onError: (error) => {
+        // TODO: redirect to pricing page if specific error
+        toast.error(error.message || "There was an error creating the project");
+      },
+    })
+  );
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    await createProject.mutateAsync({
+      value: values.value,
+    });
+  };
+
+  const isPending = createProject.isPending;
+  const isButtonDisabled = isPending || !form.formState.isValid;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+          isFocused && "shadow-xs"
+        )}
+      >
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <TextareaAutoSize
+              disabled={isPending}
+              className="pt-4 resize-none border-none w-full outline-none bg-transparent"
+              placeholder="What would you like to build today ?"
+              {...field}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              minRows={2}
+              maxRows={8}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                  e.preventDefault();
+                  form.handleSubmit(onSubmit)(e);
+                }
+              }}
+            />
+          )}
+        />
+        <div className="flex gap-x-2 items-end justify-between pt-2">
+          <div className="text-[10px] text-muted-foreground font-mono">
+            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ">
+              <span>&#8984;</span>Enter
+            </kbd>
+            &nbsp;To Submit
+          </div>
+          <Button
+            disabled={isButtonDisabled}
+            className={cn(
+              "size-8 rounded-full",
+              isButtonDisabled && "bg-muted-foreground border"
+            )}
+          >
+            {isPending ? (
+              <Loader2Icon className="size-4 animate-spin" />
+            ) : (
+              <ArrowUpIcon />
+            )}
+          </Button>
+        </div>
+      </form>
+      <div className="flex-wrap justify-center gap-2 hidden md:flex max-w-3xl"></div>
+    </Form>
+  );
+};
